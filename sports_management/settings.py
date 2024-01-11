511c5b3f8a5b0b83852b2d0cfd034c266a9a3e79
JOHNGT2018 <john.thattil@msds.christuniversity.in>
2024-01-11 08:58:39 +0530

    updates

--- sports_management/settings.py
+++ sports_management/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    'admin_login',
     'sportshome',
     'staff_login',
     'student_login',
+    'achievements',
+    
 ]
 
 MIDDLEWARE = [
